0ea7519f66f0dad59a61317334966e07f7648cc2
Felipe Faria <felipedsf@gmail.com>
2021-03-28 15:36:24 -0300

    add-backend-terrafrom

--- main.tf
+++ main.tf
@@ -2,9 +2,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "felipedsf-tf-state-de"
+    key    = "main/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 resource "aws_s3_bucket" "bucket_data_engineering_2021" {
   bucket = "felipedsf-bucket-2021"
   acl    = "private"
-
-  tags = var.tags
+  tags   = var.tags
 }
+
